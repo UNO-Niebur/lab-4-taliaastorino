b7c6466f830a8fae150152049bb618fabf944562
taliaastorino <tastorino@unomaha.edu>
2026-02-15 17:11:34 -0600

    Update TurtleGraphics.py
    Concentric squares and corner filled square

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,27 +1,59 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name: Talia Astorino
+#Date: 02/15/2026
+#Purpose: Using turtle graphics commands, algorithms with loops, and writing reusable functions.
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
-
 def drawSquare(myTurtle, size):
     for i in range(4):
         myTurtle.forward(size)
         myTurtle.right(90)
-
-
+        
+def drawPolygon(bob, sides):
+    for s in range(sides):
+        bob.forward(50)
+        bob.right(360/sides)
+        
+def fillCorner(alice, corner):
+    #Draw big square
+    drawSquare(alice, 100)
+    
+    if corner == 1:
+        alice.begin_fill()
+        drawSquare(alice,50)
+        alice.end_fill()
+    elif corner == 2:
+        alice.forward(50)
+        alice.begin_fill()
+        drawSquare(alice,50)
+        alice.end_fill()
+def squaresInSquares(myTurtle, num):
+    size = 200
+    
+    for i in range(num):
+        drawSquare(myTurtle, size)
+        
+        myTurtle.penup()
+        myTurtle.forward(10)
+        myTurtle.right(90)
+        myTurtle.forward(10)
+        myTurtle.left(90)
+        myTurtle.pendown()
+        
+        size -= 20
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
-
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
-
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
-
+    myTurtle.speed(5)
+    
+    squaresInSquares(myTurtle, 5)
+    
+    myTurtle.penup()
+    myTurtle.goto(-125, -100)
+    myTurtle.pendown()
+    
+    fillCorner(myTurtle, 2)
+    
+    turtle.done()
 
 main()
